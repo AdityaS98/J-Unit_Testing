aeb3f4b9be70938b1717ae43b3c09274d5c3c6cb
AdityaS98 <adityasharmalko1998@gmail.com>
2023-05-22 16:41:21 +0530

    Added : A Programme for testing Last Name

--- src/main/java/org/junit/RegexJava.java
+++ src/main/java/org/junit/RegexJava.java
@@ -5,14 +5,27 @@ import java.util.regex.Pattern;
 
 public class RegexJava {
 
-    public static boolean firstName(String firstName){
+    public static boolean firstName(String firstName) {
         /*
          * To define regex for first name and matching first name with the give expression for the first three letters
          */
-            Pattern obj = Pattern.compile("\\p{Upper}(\\p{Lower}+\\s?){3}");
-            return obj.matcher(firstName).matches();
+        Pattern obj = Pattern.compile("\\p{Upper}(\\p{Lower}+\\s?){3}");
+        return obj.matcher(firstName).matches();
 
 
     }
 
+    /*
+     * To define regex for last name and matching last name with the give expression for the first three letters
+     */
+
+    public static boolean lastName(String lastName) {
+        /*
+         * To define regex for first name and matching first name with the give expression for the first three letters
+         */
+        Pattern obj = Pattern.compile("\\p{Upper}(\\p{Lower}+\\s?){2}\", Name");
+        return obj.matcher(lastName).matches();
+
+
+    }
 }
--- src/test/java/JunitAssertTest.java
+++ src/test/java/JunitAssertTest.java
@@ -5,14 +5,20 @@ import org.junit.Test;
 public class JunitAssertTest {
 
     @Test
-    public void When_Given_Name_Is_Correct_ReturnTrue(){
+    public void When_Given_Name_Is_Correct_ReturnTrue() {
 
         RegexJava obj = new RegexJava();
-        boolean result=obj.firstName("Aditya");
+        boolean result = obj.firstName("Aditya");
         Assert.assertTrue(result);
 
 
-
-
     }
+
+    @Test
+    public void When_Given_LastName_Is_Correct_ReturnTrue(){
+        RegexJava obj = new RegexJava();
+        boolean result = obj.firstName("Sharma");
+        Assert.assertTrue(result);
+    }
+
 }
