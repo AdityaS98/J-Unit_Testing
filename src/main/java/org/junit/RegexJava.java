17faea116381dda5f9c800eeb119e4dbc9430632
AdityaS98 <adityasharmalko1998@gmail.com>
2023-05-22 17:04:45 +0530

    Added : A Programme for testing Email

--- src/main/java/org/junit/RegexJava.java
+++ src/main/java/org/junit/RegexJava.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class RegexJava {
 
-    public static boolean firstName(String firstName) {
+    public  boolean firstName(String firstName) {
         /*
          * To define regex for first name and matching first name with the give expression for the first three letters
          */
@@ -19,13 +19,24 @@ public class RegexJava {
      * To define regex for last name and matching last name with the give expression for the first three letters
      */
 
-    public static boolean lastName(String lastName) {
+    public  boolean lastName(String lastName) {
         /*
          * To define regex for first name and matching first name with the give expression for the first three letters
          */
-        Pattern obj = Pattern.compile("\\p{Upper}(\\p{Lower}+\\s?){2}\", Name");
+        Pattern obj = Pattern.compile("\\p{Upper}(\\p{Lower}+\\s?){2}\", lastName");
         return obj.matcher(lastName).matches();
 
 
     }
+
+    public  boolean eMail(String Email) {
+        /*
+         * To define regex for email and matching email with the give expression
+         */
+        Pattern obj = Pattern.compile("[a-z]+[.]?[a-z]{0,}[@][a-z]+[.][a-z]{2,4}[.]?[a-z]{0,2}");
+        return obj.matcher(Email).matches();
+
+
+    }
+
 }
--- src/test/java/JunitAssertTest.java
+++ src/test/java/JunitAssertTest.java
@@ -17,8 +17,14 @@ public class JunitAssertTest {
     @Test
     public void When_Given_LastName_Is_Correct_ReturnTrue(){
         RegexJava obj = new RegexJava();
-        boolean result = obj.firstName("Sharma");
+        boolean result = obj.lastName("Sharma");
         Assert.assertTrue(result);
     }
 
+    @Test
+    public void When_Given_Email_Is_Correct_ReturnTrue(){
+        RegexJava obj = new RegexJava();
+        boolean result = obj.eMail("abc.xyz@bl.co.in");
+        Assert.assertTrue(result);
+    }
 }
